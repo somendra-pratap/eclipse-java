7fefb758200b1aa1dcd8854d890bbe5c9e89049c
Somendra <somendra.pratapsingh.15@gmail.com>
2020-11-28 18:46:47 +0000

    ssh keys testing

--- src/main/java/org/eclipse/che/examples/HelloWorld.java
+++ src/main/java/org/eclipse/che/examples/HelloWorld.java
@@ -6,7 +6,7 @@ package org.eclipse.che.examples;
 
 public class HelloWorld {
     public static void main(String... argvs) {
-        String a = "Che";
+        String a = "Eclipse";
         System.out.println("Hello World " + a + "!");
     }
 }
